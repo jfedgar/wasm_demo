a7461ce2b29855087dfb830ae3c3823a15a7c703
jfedgar <jfedgar@gmail.com>
2019-05-07 01:00:12 -0400

    web assembly importing functions, logging, etc

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 
 // serve static files from public dir
-app.use(express.static('public'));
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    //serve .wasm files with correct content type
+    if (path.endsWith('.wasm')) {
+      res.set('Content-Type', 'application/wasm');
+    }
+  }
+}));
 
 app.listen(2222, () => console.log("server running on port 2222"));
